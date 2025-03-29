ffc9e5e0d46f6cce1f6588016c63df3529b62269
Air-KS <kevinrogeret@gmail.com>
2025-03-29 01:13:26 +0100

    test back for railway

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,24 +1,24 @@
-/*
-  ./backend/models/index.js
-*/
-
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// Utilise directement les variables d'environnement
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: process.env.NODE_ENV === 'development' ? console.log : false
+  }
+);
 
+// Dynamique pour charger les modèles
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -31,11 +31,10 @@ fs
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
-    db[modelName].associate(db); // Appel des méthodes d’association définies dans les modèles
+    db[modelName].associate(db);
   }
 });
 
-// Synchronisation de la base de données au besoin
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
