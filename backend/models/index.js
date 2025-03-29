9866678afe1c97d41f20eea9513cc60597ae0554
Air-KS <kevinrogeret@gmail.com>
2025-03-29 01:02:58 +0100

    test back for railway

--- backend/models/index.js
+++ backend/models/index.js
@@ -7,17 +7,21 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+require('dotenv').config();
+
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: process.env.NODE_ENV === 'development' ? console.log : false
+  }
+);
 
 fs
   .readdirSync(__dirname)
@@ -31,11 +35,10 @@ fs
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
-    db[modelName].associate(db); // Appel des méthodes d’association définies dans les modèles
+    db[modelName].associate(db);
   }
 });
 
-// Synchronisation de la base de données au besoin
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
