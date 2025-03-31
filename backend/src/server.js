dc29c28ecaf09875997acbf6e0ad48b0a4529849
Air-KS <kevinrogeret@gmail.com>
2025-03-31 10:11:04 +0200

    test for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -62,7 +62,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
     httpOnly: true,
     sameSite: 'lax'
   }
