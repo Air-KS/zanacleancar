9f8935ff226d1da9ac6a9c1ec388253853da73be
Air-KS <kevinrogeret@gmail.com>
2025-04-01 17:55:29 +0200

    test for deployment

--- frontend/portal/src/views/profilPage.vue
+++ frontend/portal/src/views/profilPage.vue
@@ -130,31 +130,24 @@ export default {
       this.toast?.success("🎉 Toast injecté et fonctionnel !");
     },
     async fetchUserProfil() {
-      const userId = parseInt(this.$route.params.id);
+  const userId = parseInt(this.$route.params.id);
 
-      try {
-        const response = await axios.get(
-          `${import.meta.env.VITE_API_URL}/api/v1/user/profil/${userId}`,
-          { withCredentials: true }
-        );
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/api/v1/user/profil/${userId}`,
+      { withCredentials: true }
+    );
 
-        this.user = response.data;
+    if (response.data.redirect && response.data.ownId !== userId) {
+      window.location.href = `/profil/${response.data.ownId}`;
+      return;
+    }
 
-      } catch (error) {
-        if (error.response && error.response.status === 403) {
-          const store = useUserStore();
-          const ownId = store.user?.id;
-
-          if (ownId && ownId !== userId) {
-            this.$router.push(`/profil/${ownId}`).then(() => {
-              this.fetchUserProfil(); // recharge les bonnes données après redirection
-            });
-          } else {
-            this.$router.push('/login');
-          }
-        }
-      }
-    },
+    this.user = response.data;
+  } catch {
+    window.location.href = '/login';
+  }
+},
     async handleSave() {
       const userId = this.$route.params.id;
       try {
