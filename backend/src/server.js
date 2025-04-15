0fc11ad70afa2e3edfb847d50a29a1f7178656a5
Air-KS <kevinrogeret@gmail.com>
2025-04-15 17:37:04 +0200

    [Testing] - debug profil safari on ios

--- backend/src/server.js
+++ backend/src/server.js
@@ -88,6 +88,10 @@ sessionStore.sync();
 app.use((req, res, next) => {
   console.log('🔍 Cookie reçu:', req.headers.cookie);
   console.log('🔐 Session:', req.session);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
 
