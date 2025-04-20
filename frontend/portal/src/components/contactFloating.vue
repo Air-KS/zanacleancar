9e633fc136b6f37541c4b6f4a3b2db1fc79fa9e6
Air-KS <kevinrogeret@gmail.com>
2025-04-20 14:55:56 +0200

    [Contact] - Menu flottant pour contact

--- frontend/portal/src/components/contactFloating.vue
+++ frontend/portal/src/components/contactFloating.vue
@@ -7,7 +7,7 @@
     <transition name="fade">
       <div v-show="isOpen" class="contact-menu">
         <p>📞 <strong>Téléphone :</strong> <a href="tel:0610421272">06.10.42.12.72</a></p>
-        <p>📧 <strong>Email :</strong> <a href="mailto:zanacleancar@gmail.com">zanacleancar@gmail.com</a></p>
+        <p>📧 <strong>Email :</strong> <a href="mailto:contact@zanacleancar.fr">contact@zanacleancar.fr</a></p>
         <p>📍 <strong>Zone :</strong> Roubaix & Alentours</p>
         <div class="map">
           <iframe src="https://www.google.com/maps?q=Roubaix,France&output=embed" allowfullscreen="" loading="lazy">
