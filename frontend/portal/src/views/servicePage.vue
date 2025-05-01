03925c3431b1677adfee5a9aa758f33bd7fd99a4
Air-KS <kevinrogeret@gmail.com>
2025-05-02 00:42:08 +0200

    [update - service page] - Modifie la page pour ajouter de nouveaux pack

--- frontend/portal/src/views/servicePage.vue
+++ frontend/portal/src/views/servicePage.vue
@@ -17,7 +17,7 @@
 
     <p class="sub-title">Nos Pack</p>
     <p style="text-align: center; font-size: 15px">
-      <strong>🛻 Déplacement : 💶 10€ / 30 min à partir de Roubaix</strong>
+      <strong>🛻 Déplacement : 💶 10€ / 25 min à partir de Roubaix</strong>
     </p>
     <br />
     <div class="arrow-down">
@@ -29,7 +29,7 @@
       <article class="service-card">
         <div class="service-header">
           <div class="service-info">
-            <img class="service-icon" src="@/assets/Pack/bronze.png" alt="Pack Bronze" />
+            <img class="service-icon" src="@/assets/Pack/badge-eclair.png" alt="Pack Éclat" />
             <h2 class="service-title">Pack Éclat</h2>
           </div>
           <span class="service-price">💶 60€</span>
@@ -38,8 +38,8 @@
         <p><strong>Intérieur complet – Léger entretien</strong></p>
         <p>Parfait pour les véhicules du quotidien qui ont simplement besoin d’un petit rafraîchissement.</p>
         <p>Ce pack est idéal si tu veux retrouver un habitacle propre et agréable sans y consacrer trop de temps.</p>
-        <p>C’est le bon compromis entre rapidité et efficacité pour garder ta voiture toujours présentable.
-          Un service discret mais qui fait toute la différence au quotidien.</p>
+        <p>C’est le bon compromis entre rapidité et efficacité pour garder ta voiture toujours présentable.</p>
+        <p><strong>Un service discret mais qui fait toute la différence au quotidien.</strong></p>
         </p>
       </article>
 
@@ -47,7 +47,7 @@
       <article class="service-card">
         <div class="service-header">
           <div class="service-info">
-            <img class="service-icon" src="@/assets/Pack/bronze.png" alt="Pack Bronze" />
+            <img class="service-icon" src="@/assets/Pack/badge-eau.png" alt="Pack Brillance" />
             <h2 class="service-title">Pack Brillance</h2>
           </div>
           <span class="service-price">💶 90€</span>
@@ -59,7 +59,7 @@
         <p>Ce pack va bien plus loin qu’un simple rafraîchissement : il redonne une vraie allure à ton intérieur, avec
           une finition soignée et durable.</p>
         <p>Idéal si tu veux retrouver le plaisir d’un habitacle net, propre, accueillant.</p>
-        <p>Une transformation notable, sans aller jusqu’à la rénovation complète.</p>
+        <p><strong>Assez complet pour te faire redécouvrir ton habitacle sous un nouveau jour.</strong></p>
         </p>
       </article>
 
@@ -67,7 +67,7 @@
       <article class="service-card">
         <div class="service-header">
           <div class="service-info">
-            <img class="service-icon" src="@/assets/Pack/bronze.png" alt="Pack Bronze" />
+            <img class="service-icon" src="@/assets/Pack/badge-feu.png" alt="Pack Préstige" />
             <h2 class="service-title">Pack Préstige</h2>
           </div>
           <span class="service-price">💶 130€</span>
@@ -79,7 +79,7 @@
           animaux ou les oublis.</p>
         <p>Ce pack va au bout des choses : chaque recoin est traité avec minutie pour un résultat spectaculaire.</p>
         <p>Si tu veux rouler dans une voiture propre comme au premier jour, c’est ce qu’il te faut.</p>
-        <p><strong>Un nettoyage poussé, visible, radical.</strong></p>
+        <p><strong>Un nettoyage profond, pensé pour transformer l’intérieur, pas juste le nettoyer.</strong></p>
         </p>
       </article>
 
@@ -87,7 +87,7 @@
       <article class="service-card">
         <div class="service-header">
           <div class="service-info">
-            <img class="service-icon" src="@/assets/Pack/bronze.png" alt="Pack Bronze" />
+            <img class="service-icon" src="@/assets/Pack/badge-etoile.png" alt="Pack Spécial Avant-Vente" />
             <h2 class="service-title">Pack Spécial Avant-Vente</h2>
           </div>
           <span class="service-price">💶 170€</span>
@@ -99,27 +99,28 @@
           portes.</p>
         <p>Chaque détail compte : propreté, odeur, aspect visuel… tout est fait pour inspirer confiance et créer le
           coupde cœur.</p>
-        <p><strong>Un vrai coup de boost à la revente, sans tricher.</strong></p>
+        <p><strong>Un véritable argument de vente, visuel, propre, prêt à convaincre.</strong></p>
         </p>
       </article>
 
-      <p class="sub-title" style="padding-top: 50px">💼 Petits budgets & entretiens rapides</p>
+      <p class="sub-title" style="padding-top: 50px">💼 Petits budgets & Entretiens rapides</p>
 
       <p>💡 Ces packs express sont pensés pour les besoins simples ou ponctuels.
-        Pas de nettoyage en profondeur ici — juste l’essentiel pour garder votre voiture présentable au quotidien.</p>
+        Pas de nettoyage en profondeur ici — Juste l’essentiel pour garder votre voiture présentable au quotidien.</p>
+
       <!-- Service 5 - Pack Bronze -->
       <article class="service-card">
         <div class="service-header">
           <div class="service-info">
-            <img class="service-icon" src="@/assets/Pack/bronze.png" alt="Pack Bronze" />
+            <img class="service-icon" src="@/assets/Pack/badge-plante.png" alt="Pack Express" />
             <h2 class="service-title">Pack Express ~ 25m 🕒</h2>
           </div>
           <span class="service-price">💶 25€</span>
         </div>
         <p class="service-description">
-          <p>Rapide et efficace pour un coup de propre immédiat.</p>
-          <p>Parfait pour les gens pressés ou les entretiens réguliers.</p>
-          <p><strong><em>⚠️ Ne comprend pas les prestations complètes des grands packs.</em></strong></p>
+        <p>Rapide et efficace pour un coup de propre immédiat.</p>
+        <p>Parfait pour les gens pressés ou les entretiens réguliers.</p>
+        <p><strong><em>⚠️ Ne comprend pas les prestations complètes des grands packs.</em></strong></p>
         </p>
         <ul class="service-features">
           <li>
@@ -129,19 +130,21 @@
           <li>✔️ Nettoyage rapide des plastiques</li>
         </ul>
       </article>
+
       <!-- Service 6 - Pack Gold -->
       <article class="service-card">
         <div class="service-header">
           <div class="service-info">
-            <img class="service-icon" src="@/assets/Pack/gold.png" alt="Pack Gold" />
+            <img class="service-icon" src="@/assets/Pack/badge-vent.png" alt="Pack Confort" />
             <h2 class="service-title">Pack Confort ~ 45m 🕒</h2>
           </div>
           <span class="service-price">💶 35€</span>
         </div>
         <p class="service-description">
-          <p>Un nettoyage simple et soigné, pour garder un intérieur agréable sans y passer des heures.</p>
-<p>Idéal pour les véhicules du quotidien, ce pack couvre les bases essentielles sans traitement en profondeur.</p>
-<p><strong><em>⚠️ Ne comprend pas les prestations complètes des grands packs.</em></strong></p>
+        <p>Un nettoyage simple et soigné, pour garder un intérieur agréable sans y passer des heures.</p>
+        <p>Idéal pour les véhicules du quotidien, ce pack couvre les bases essentielles sans traitement en profondeur.
+        </p>
+        <p><strong><em>⚠️ Ne comprend pas les prestations complètes des grands packs.</em></strong></p>
         </p>
         <ul class="service-features">
           <li>
@@ -158,15 +161,16 @@
       <article class="service-card">
         <div class="service-header">
           <div class="service-info">
-            <img class="service-icon" src="@/assets/Pack/diamond.png" alt="Pack Diamond" />
+            <img class="service-icon" src="@/assets/Pack/badge-terre.png" alt="Pack Premium" />
             <h2 class="service-title">Pack Premium ~ 60m 🕒</h2>
           </div>
           <span class="service-price">💶 45€</span>
         </div>
         <p class="service-description">
-          <p>Pensé pour rafraîchir un habitacle visiblement sale, sans aller jusqu’à la rénovation totale.</p>
-          <p>Un bon compromis pour les petits budgets qui veulent un résultat propre et net.</p>
-          <p><strong><em>⚠️ Ce pack reste une version allégée, sans nettoyage intégral ou ultra-détaillé.</em></strong></p>
+        <p>Pensé pour rafraîchir un habitacle visiblement sale, sans aller jusqu’à la rénovation totale.</p>
+        <p>Un bon compromis pour les petits budgets qui veulent un résultat propre et net.</p>
+        <p><strong><em>⚠️ Ce pack reste une version allégée, sans nettoyage intégral ou ultra-détaillé.</em></strong>
+        </p>
         </p>
         <ul class="service-features">
           <li>
@@ -182,6 +186,35 @@
           <li>✔️ Carrosserie intérieure</li>
         </ul>
       </article>
+
+      <p class="sub-title" style="padding-top: 50px">🎁 Pack Fidélité – Ton nettoyage offert</p>
+
+      <p>💡 Ton habitude d’entretien est récompensée : compose ton propre service, c’est mérité !</p>
+
+      <!-- Service 8 - Pack Cosmos -->
+      <article class="service-card">
+        <div class="service-header">
+          <div class="service-info">
+            <img class="service-icon" src="@/assets/Pack/badge-cosmos.png" alt="Pack Cosmos" />
+            <h2 class="service-title">Pack Cosmos – Offert</h2>
+          </div>
+          <span class="service-price">💶 0€</span>
+        </div>
+        <p class="service-description">
+        <p>Un service personnalisé, offert (après 7 lavages).</p>
+        <p>Le sourire sur ta carte de fidélité, nous le confirme : Tu mérites ce pack personnalisé !</p>
+        <p><strong>Compose ton propre service : choisis 1 prestation dans chaque duo :</strong></p>
+        </p>
+        <ul class="service-features">
+          <li>✔️ Aspiration ou Shampouineuse <span class="stars" data-stars="2"></span></li>
+          <li>✔️ Tapis ou moquette</li>
+          <li>✔️ Tableau de bord ou plastique</li>
+          <li>✔️ Portes ou Vitres</li>
+        </ul><br>
+        <p>🛠️ Sois créatif, choisis ce qui compte pour toi.</p>
+        <p><strong>Nous, on assure derrière — avec la même qualité que n’importe quel pack.</strong></p>
+      </article>
+
     </section>
 
     <!-- ========================================
@@ -316,12 +349,12 @@
       <article class="service-item">
         <div class="service-header">
           <div class="service-title">
-            <span class="service-name" style="margin-left: 10px">✔️ Carte de Fidélité ➕ Loyalties</span>
+            <span class="service-name" style="margin-left: 10px">✔️ Carte de Fidélité ➕ Loyauté</span>
           </div>
         </div>
         <div class="service-desc">
-          <p>* 7 nettoyages = 1 Pack Express OFFERT</p>
-          <p>* 2 points de Loyalties / €</p>
+          <p>* 7 tampons = 1 Pack Cosmos OFFERT</p>
+          <p>* 2 points par € dépensé → échangeables contre des bonus ou avantages</p>
         </div>
       </article>
     </section>
